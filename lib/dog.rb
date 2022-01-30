4de0acf42cfd51660e0a3ec9361f778ab6a4779f
seth <seth623@gmail.com>
2022-01-30 15:25:22 -0700

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog
+
+    @@all = [] 
+
+    def initialize(name)
+        @name = name
+        save 
+    end 
+
+    attr_accessor :name
+
+    def save 
+        @@all << self 
+    end
+
+    def self.all 
+        @@all 
+    end 
+
+    def self.print_all
+        @@all.each { |dog| puts "#{dog.name}"}
+    end 
+
+    def self.clear_all
+        @@all.clear 
+    end 
+
+end
